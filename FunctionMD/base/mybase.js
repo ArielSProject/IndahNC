c344364ef508683bd2acd1da745b96dcab11dc28
ArielSProject <108188273+ArielSProject@users.noreply.github.com>
2022-07-25 10:57:00 +0700

    Update mybase.js

--- FunctionMD/base/mybase.js
+++ FunctionMD/base/mybase.js
@@ -122,22 +122,22 @@
   const times = moment().tz('Asia/Jakarta').format('HH:mm:ss')
   if(times < "23:59:00"){
     m.sayingtime         = 'Selamat Malam'
-    m.timoji = 'ðŸŒƒ'}
+    m.timoji = '🌃'}
   if(times < "19:00:00"){
     m.sayingtime         = 'Selamat Sore'
-    m.timoji = 'ðŸŒ†'}
+    m.timoji = '🌇'}
   if(times < "18:00:00"){
     m.sayingtime         = 'Selamat Sore'
-    m.timoji = 'ðŸŒ‡'}
+    m.timoji = '🌇'}
   if(times < "15:00:00"){
     m.sayingtime         = 'Selamat Sore'
-    m.timoji = 'ðŸŒž'}
+    m.timoji = '🌇'}
   if(times < "11:00:00"){
     m.sayingtime         = 'Selamat Pagi'
-    m.timoji = 'ðŸŒ…'}
+    m.timoji = '🌅'}
   if(times < "05:00:00"){
     m.sayingtime         = 'Selamat Malam'
-    m.timoji = 'ðŸŒƒ' }  
+    m.timoji = '🌃' }  
 
     m.tanggal10 = new Date('1 May 2022 00:00:00').getTime();
     m.sekarang1 = new Date().getTime();
